54fd265cf1a8fe84d028c763569d13da76e632f9
ZMS-ELE <102344119+ZMS-ELE@users.noreply.github.com>
2022-08-15 21:15:35 +0800

    Add files via upload

---
+++ 1.h
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<stdlib.h>
+#include<time.h>
+#include<cstdio>
+#include<windows.h>
+#include<conio.h>
+void dqsz();
+void help();
+void kfzms();
+void ks();
+void ksa();
+int i, j, life, maxrand, c;
